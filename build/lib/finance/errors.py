6e7efa344ab6f94647730801eab62cb85a5b341c
f0hy <sashkaloel@yandex.ru>
2026-07-27 20:25:36 +0500

    remove package build artifacts

--- build/lib/finance/errors.py
+++
@@ -1,6 +0,0 @@
-class DomainError(ValueError):
-    """A client-correctable error in a financial operation."""
-
-
-class NotFoundError(DomainError):
-    """Raised when a referenced financial entity does not exist."""
